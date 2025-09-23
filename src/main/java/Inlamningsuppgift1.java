8f0c7e97980e20fc6f6ddeb4c74649c0e55f070c
ssandramedina <ssandramedina@hotmail.com>
2025-09-24 00:12:16 +0200

    hej

--- src/main/java/Inlamningsuppgift1.java
+++ src/main/java/Inlamningsuppgift1.java
@@ -70,29 +70,3 @@ public class Inlamningsuppgift1 {
 
     }
 }
-
-
-class Spelare {
-    private String firstname;
-    private String lastname;
-    private int score;
-
-    public Spelare(String firstname, String lastname) {
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.score = 0;
-
-    }
-
-    public void addToScore(int scoreToAdd) {
-        this.score += scoreToAdd;
-    }
-
-    public String getFullName() {
-        return firstname + " " + lastname;
-    }
-
-    public int getScore() {
-        return score;
-    }
-}
